7138a5794ed44c06f1a0e5ef626201a7a771c50f
PeeblyWeeb <peebly@salamithecat.com>
2026-07-02 23:32:29 -0400

    fixfixfix: i don't wanna talk about it

--- src/main/java/com/salamithecat/chatChannels/Listeners/ChannelEventListener.java
+++ src/main/java/com/salamithecat/chatChannels/Listeners/ChannelEventListener.java
@@ -64,8 +64,12 @@ public class ChannelEventListener implements Listener {
 
             ChatRenderer previousRenderer = event.renderer();
             event.renderer(((source, sourceDisplayName, message, viewer) -> {
-                PersistentDataContainer renderDataContainer = source.getPersistentDataContainer();
-                boolean currentHiddenValue = Optional.ofNullable(renderDataContainer.get(Constants.ChannelHiddenKey, PersistentDataType.BOOLEAN)).orElse(false);
+                boolean currentHiddenValue = false;
+
+                if (viewer instanceof Player renderPlayer) {
+                    PersistentDataContainer renderDataContainer = renderPlayer.getPersistentDataContainer();
+                    currentHiddenValue = Optional.ofNullable(renderDataContainer.get(Constants.ChannelHiddenKey, PersistentDataType.BOOLEAN)).orElse(false);
+                }
 
                 Component rendered = previousRenderer.render(source, sourceDisplayName, message, viewer);
                 return Component.text("")
